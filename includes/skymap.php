54f3653bc6b9cd87303fca8bd8cc12f6c6c56ed4
Manu Marchal <manumarchal@users.noreply.github.com>
2020-03-08 01:23:07 +0000

    fix height

--- includes/skymap.php
+++ includes/skymap.php
@@ -10,7 +10,7 @@ function DisplaySkyMap(){
       <div class="panel panel-primary">
         <div class="panel-heading"><i class="fa fa-star fa-fw"></i>Local sky</div>
         <div class="panel-body">
-	  	<iframe width="100%" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" 		src="https://virtualsky.lco.global/embed/index.html?longitude=-0.18&latitude=51&projection=polar&magnitude=10" allowTransparency="false"></iframe>
+	  	<iframe height="500" width="100%" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" 		src="https://virtualsky.lco.global/embed/index.html?longitude=-0.18&latitude=51&projection=polar&magnitude=10" allowTransparency="false"></iframe>
         </div><!-- /.panel-body -->
       </div><!-- /.panel-default -->
     </div><!-- /.col-lg-12 -->
