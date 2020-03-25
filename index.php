1d8b86fd6589c21deacca9a49f88fcb6ee39b2e2
Manu Marchal <manumarchal@users.noreply.github.com>
2020-03-25 10:38:20 +0000

    Update index.php

--- index.php
+++ index.php
@@ -110,7 +110,7 @@ $csrf_token = $_SESSION['csrf_token'];
         <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
         <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
     <![endif]-->
-    <!--<meta property="og:title" content="All Sky Camera" />
+    <!--[<meta property="og:title" content="All Sky Camera" />
     <meta property="og:type" content="image/jpeg" />
     <meta property="og:url" content="http://www.thomasjacquin/allsky/" />
     <meta property="og:image" content="http://www.thomasjacquin.com/allsky/image.jpg" />
